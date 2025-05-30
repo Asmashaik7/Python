9b4b584185125642ba21e98dd71f8dd5f16b6507
Asmashaik7 <asmashaik.nov11@gmail.com>
2025-05-30 18:41:50 +0530

    Update Funct_args_as_inputs.py

--- Funct_args_as_inputs.py
+++ Funct_args_as_inputs.py
@@ -6,7 +6,7 @@ def interview2(name,yob,city,edu,gender,contact):
     details['name']=name.upper()
     details['gender']=gender[0].upper()
     details['eduaction']=edu.capitalize()
-    details['contact']=+91+contact
+    details['contact']=+91+int(contact)
     details['age']=current_year-int(yob)        
     return details
     
@@ -14,6 +14,5 @@ interview2('sri',2010,'wgl','bsc','male',1234567)
 interview2('asma',2011,'kgm','bsc','female',456738)
 interview2('manha',2012,'bdcr kgm','mba','female',4534538)
 interview2('danish',2013,'hyd','mca','male',99894538)
-details
 
 
